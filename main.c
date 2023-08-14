87817f5cbc00753fa83990ff4937fb940cc1ce8d
MiroLuis <miroluis@gmail.com>
2023-08-14 16:11:03 +0100

    it make sum

--- main.c
+++ main.c
@@ -1,22 +1,26 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
 int iPrintStr(char *c)
 {
     printf(c);
 }
 
-int iUpdateStr(char *fraseEcra, char *valor)
-{
+int iPrintLinha(char *fraseEcra){
     printf("#");
-    strcat(fraseEcra, valor);
-    fraseEcra[strlen(fraseEcra)-1]='\0';
     for (size_t i = 0; i < (16 - strlen(fraseEcra)); i++)
     {
         printf(" ");
     }
     printf(fraseEcra);
     printf("#\n");
+}
+
+int iUpdateStr(char *fraseEcra, char *valor)
+{
+    strcat(fraseEcra, valor);
+    fraseEcra[strlen(fraseEcra)-1]='\0';
 }
 
 int main()
@@ -27,13 +31,17 @@ int main()
     cValor[0] = '\0'; //limpa entrada de dados
     oldValue[0] = '\0';
     newValue[0] = '\0';
+    char operation = '\0';
+
+    iUpdateStr(oldValue, "");
 
     while(cValor[0] != 'q')
     {
         //update screen
         printf("##################\n");
-        iUpdateStr(oldValue, "");
+        iPrintLinha(oldValue);
         iUpdateStr(newValue, cValor);
+        iPrintLinha(newValue);
         printf("##################\n");
 
 
@@ -43,8 +51,24 @@ int main()
         {
             cValor[0] = '\0';
             strcpy(oldValue, newValue);
-            newValue[0] = '\n';
+            newValue[0] = '\0';
+            operation = '+';
+
+        } else if (cValor[0] == '=')
+        {
+            int ioldValue = atoi(oldValue);
+            printf("O oldValue %d\n", ioldValue);
+            int inewValue = atoi(newValue);
+            printf("O newValue %d\n", inewValue);
+
+            int total = ioldValue + inewValue;
+
+            oldValue[0] = '\0';
+            sprintf(oldValue, "%d", total);
+            
+            //iUpdateStr(oldValue, cValor);
         }
+        
         
         printf(" cValor = %s\n", cValor);
     } 
